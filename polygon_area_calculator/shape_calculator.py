c740751ec61427487c8e0a56772d95abe0606f68
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2023-03-28 13:43:51 -0700

    Add files via upload
    added polygon area calculator

---
+++ polygon_area_calculator/shape_calculator.py
@@ -0,0 +1,57 @@
+class Rectangle:
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+    
+  def set_width(self, width):
+    self.width = width
+
+  def set_height(self, height):
+    self.height = height
+    
+  def get_area(self):
+    return self.width * self.height
+
+  def get_perimeter(self):
+    return (2 * self.width + 2 * self.height)
+
+  def get_diagonal(self):
+     return ((self.width ** 2 + self.height ** 2) ** .5)
+
+  def get_picture(self):
+    if self.width > 50 or self.height > 50:
+      return "Too big for picture."
+    else:
+      str = ""
+      for i in range(self.height):
+        for j in range(self.width):
+          str += "*"
+        str += "\n"
+      return str
+
+  def get_amount_inside(self, shape):
+    home = self.get_area()
+    guest = shape.get_area()
+    counts = 0
+    while home >= guest:
+      home -= guest
+      counts += 1
+    return counts
+    
+  def __str__(self):
+    return f"{type(self).__name__}(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+  def __init__(self, side):
+    super().__init__(self, side)
+    self.width = side
+    self.height = side
+
+  def set_side(self, side):
+    self.set_width(side)
+    self.set_height(side)
+    
+  def __str__(self):
+    return f"{type(self).__name__}(side={self.width})"
+    
